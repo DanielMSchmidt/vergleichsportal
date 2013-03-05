d95791e29b56ae526e3e333f30b7cc379d9d2aa1
Daniel Schmidt <dschmidt@weluse.de>
2013-03-05 20:49:27 +0100

    setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,17 +1,16 @@
 Vergleichsportal::Application.configure do
-  config.after_initialize do 
+  config.after_initialize do
 
-  Bullet.enable = true 
+  Bullet.enable = true
 
-  Bullet.alert = true 
+  Bullet.alert = true
 
-  Bullet.bullet_logger = true 
+  Bullet.bullet_logger = true
 
-  Bullet.console = true 
+  Bullet.console = true
 
-  Bullet.rails_logger = true 
+  Bullet.rails_logger = true
 
-  Bullet.disable_browser_cache = true 
 
 end
   # Settings specified here will take precedence over those in config/application.rb
