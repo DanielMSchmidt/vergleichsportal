1e0d0d304588c0842db92fbf71bd616ee2259ccf
Daniel Schmidt <dschmidt@weluse.de>
2013-03-08 11:37:50 +0100

    added validation for advertisment

--- spec/models/advertisment_spec.rb
+++ spec/models/advertisment_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper'
 
 describe Advertisment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation of attributes" do
+
+    it "should be an image_url present" do
+      should validate_presence_of(:img_url)
+    end
+
+    it "should be a link_url present" do
+      should validate_presence_of(:link_url)
+    end
+    describe "should be an url in image_url" do
+
+      it "should be okay with an url" do
+        a = Advertisment.new(img_url:"http://tibor-weiss.de/Fotos/2012Schandmaul/Schandmaul/content/images/thumb/IMG_3432.jpg", link_url:"http://www.tibor-weiss.de", active:false)
+        a.should be_valid
+      end
+      it "shouldn't be okay with an invalid url" do
+        a = Advertisment.new(img_url:"test", link_url:"http://www.tibor-weiss.de", active:false)
+        a.should be_invalid
+      end
+    end
+
+    describe "should be an url in link_url" do
+      it "should be okay with an url" do
+        a = Advertisment.new(img_url:"http://tibor-weiss.de/Fotos/2012Schandmaul/Schandmaul/content/images/thumb/IMG_3432.jpg", link_url:"http://www.tibor-weiss.de", active:false)
+        a.should be_valid
+      end
+      it "shouldn't be okay with an invalid url" do
+        a = Advertisment.new(img_url:"http://tibor-weiss.de/Fotos/2012Schandmaul/Schandmaul/content/images/thumb/IMG_3432.jpg", link_url:"http://www.tibor-weiss", active:false)
+        a.should be_invalid
+      end
+    end
+  end
 end
