190ba6fe41c27397bf7a0ab21ae8817850da24a0
Daniel Schmidt <dschmidt@weluse.de>
2013-03-08 21:54:01 +0100

    added relationship and validation

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,7 @@
 class Role < ActiveRecord::Base
   attr_accessible :name
+
+  has_many :users
+  validates :name, presence: true
+
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Role do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "should validate presence of name" do
+      should validate_presence_of(:name)
+    end
+  end
+
+  describe "relations" do
+    it "should have many user" do
+      should have_many(:users)
+    end
+  end
 end
