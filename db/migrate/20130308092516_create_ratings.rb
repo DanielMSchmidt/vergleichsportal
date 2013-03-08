2c6ef8fef6aa62ba9ead0cec24498f9871746182
Daniel Schmidt <dschmidt@weluse.de>
2013-03-08 16:22:15 +0100

    added provider_id

--- db/migrate/20130308092516_create_ratings.rb
+++ db/migrate/20130308092516_create_ratings.rb
@@ -3,6 +3,7 @@ class CreateRatings < ActiveRecord::Migration
     create_table :ratings do |t|
       t.integer :value
       t.integer :user_id
+      t.integer :provider_id
 
       t.timestamps
     end
