1b76662390411a63b9719fbb9a5f5080a358f65c
Daniel Schmidt <dschmidt@weluse.de>
2013-03-08 16:07:53 +0100

    added tests for ratings

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Rating do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "should validate presence of user_id" do
+      should validate_presence_of(:user_id)
+    end
+  end
+
+  describe "relations" do
+    it "should belong to carts" do
+      should belong_to(:user)
+    end
+    it "should belong to provider" do
+      should belong_to(:provider)
+    end
+  end
 end
