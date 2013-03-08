ff52103d6d63fabe3f0212b3122924a370192c47
Daniel Schmidt <dschmidt@weluse.de>
2013-03-08 12:22:53 +0100

    added tests for relations of articles

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -16,11 +16,24 @@ describe Article do
   end
 
   describe "relations" do
-    it "should have many carts"
-    it "should have many comments"
-    it "should have many ratings"
-    it "should have many images"
-    it "should have many prices"
-    it "should have at least one price"
+    it "should have many carts" do
+      should have_many(:carts).through(:article_cart_relations)
+    end
+    it "should have many comments" do
+      should have_many(:comments)
+    end
+    it "should have many ratings" do
+      should have_many(:ratings)
+    end
+    it "should have many images" do
+      should have_many(:images)
+    end
+    it "should have many prices" do
+      should have_many(:prices)
+    end
+    describe "the number of prices" do
+      it "should be invalid without a price"
+      it "should be valid with a price"
+    end
   end
 end
