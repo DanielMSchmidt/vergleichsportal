2041e47a7875bbb702421f7cf4b6c7afbd476f50
Daniel Schmidt <dschmidt@weluse.de>
2013-03-08 15:48:42 +0100

    added url tests for provider

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,3 +1,8 @@
 class Provider < ActiveRecord::Base
   attr_accessible :image_url, :name, :url
+  validates :image_url, :name, :url, presence: true
+  validates :image_url, image_url: true
+  validates :url, url: true
+
+  has_many :prices
 end
--- spec/models/provider_spec.rb
+++ spec/models/provider_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Provider do
+  let(:provider) { FactoryGirl.create(:provider) }
   describe "validations" do
     it "should validate presence of image_url" do
       should validate_presence_of(:image_url)
@@ -12,8 +13,13 @@ describe Provider do
       should validate_presence_of(:url)
     end
     describe "should validate that url is a url" do
-      it "should fail if it isn't an url"
-      it "should succeed if it is an url"
+      it "should fail if it isn't an url" do
+        provider.url = "notavalidurl"
+        provider.should_not be_valid
+      end
+      it "should succeed if it is an url" do
+        provider.should be_valid
+      end
     end
 
   end
