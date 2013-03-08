2a57999389eaf8ad118ddcd3376ed28164d50f92
Daniel Schmidt <dschmidt@weluse.de>
2013-03-08 12:20:25 +0100

    added relations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,11 @@
 class Article < ActiveRecord::Base
   attr_accessible :description, :ean, :name
+  has_many :article_cart_relations
+  has_many :carts, through: :article_cart_relations
+  has_many :comments, as: :commentable
+  has_many :ratings, as: :rateable
+  has_many :images, as: :imageable
+  has_many :prices
+
+
 end
