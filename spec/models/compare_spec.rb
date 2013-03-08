81208c8d94e33e32293d08f7e0ec09b9e24b72c4
Daniel Schmidt <dschmidt@weluse.de>
2013-03-08 14:48:54 +0100

    fixed order of specs

--- spec/models/compare_spec.rb
+++ spec/models/compare_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe Compare do
   describe "validations" do
-    it "should belong to carts" do
-      should belong_to(:cart)
+    it "should validate presence of cart_id" do
+      should validate_presence_of(:cart_id)
     end
   end
 
   describe "relations" do
-    it "should validate presence of cart_id" do
-      should validate_presence_of(:cart_id)
+    it "should belong to carts" do
+      should belong_to(:cart)
     end
   end
 end
