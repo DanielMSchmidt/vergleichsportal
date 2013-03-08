970e53056b5d0991989e3ef1333cabfb00cf4df6
Daniel Schmidt <dschmidt@weluse.de>
2013-03-08 15:39:44 +0100

    examined validators

--- spec/models/provider_spec.rb
+++ spec/models/provider_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Provider do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "should validate presence of image_url" do
+      should validate_presence_of(:image_url)
+    end
+    it "should validate presence of name" do
+      should validate_presence_of(:name)
+    end
+    it "should validate presence of url" do
+      should validate_presence_of(:url)
+    end
+    describe "should validate that url is a url" do
+      it "should fail if it isn't an url"
+      it "should succeed if it is an url"
+    end
+
+  end
+
+  describe "relations" do
+    it "should belong to carts" do
+      should have_many(:prices)
+    end
+  end
 end
