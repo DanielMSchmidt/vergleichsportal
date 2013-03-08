c8646ed1d19b81862977e89bc52d9cd45c05e3fd
Daniel Schmidt <dschmidt@weluse.de>
2013-03-08 11:43:43 +0100

    added raw article test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Article do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "attributes" do
+    describe "ean" do
+      it "should be valid with right url"
+      it "should be invalid with right url"
+    end
+    describe "description" do
+      it "should be present"
+    end
+
+    describe "name" do
+      it "should be present"
+    end
+  end
+
+  describe "relations" do
+    it "should have many carts"
+    it "should have many comments"
+    it "should have many ratings"
+    it "should have many images"
+    it "should have many prices"
+    it "should have at least one price"
+  end
 end
