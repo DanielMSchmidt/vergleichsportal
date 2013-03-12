25dff182b16be660fc1f5ca72a5828b02167d8cc
Daniel Schmidt <dschmidt@weluse.de>
2013-03-12 20:50:42 +0100

    deleted ebay config

--- config/ebay_config.rb
+++
@@ -1,8 +0,0 @@
-Ebay::Api.configure do |ebay|
-  ebay.auth_token = ENV['EBAY_AUTH_TOKEN']
-  ebay.dev_id = ENV['EBAY_DEV_ID']
-  ebay.app_id = ENV['EBAY_APP_ID']
-  ebay.cert = ENV['EBAY_CERT_ID']
-
-  ebay.use_sandbox = !Rails.env.production?
-end
