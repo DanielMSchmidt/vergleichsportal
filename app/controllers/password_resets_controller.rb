f3e38857bd85787ab68a7fc6cefe45dbfb750520
Hannes Strubel <stamm-donhansen@web.de>
2013-03-14 17:06:23 +0100

    Bugfix reset passwort form

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -17,16 +17,20 @@ class PasswordResetsController < ApplicationController
   end
 
   def update
+    @user_new = User.new
     @token = params[:token]
     @user = User.load_from_reset_password_token(params[:token])
     not_authenticated unless @user
 
     @user.password_confirmation = params[:user][:password_confirmation]
 
-    if @user.change_password!(params[:user][:password])
-      redirect_to(root_path, :notice => 'Password was successfully updated.')
-    else
-      render :action => "edit"
+    respond_to do |format|
+      if @user.change_password!(params[:user][:password])
+
+        format.html {redirect_to(root_path, :notice => 'Password was successfully updated.')}
+      else
+        format.html { render :action => "edit"}
+      end
     end
   end
 end
