59059b634c58eb0db0fa4640af772de920fd8723
David <davide135@gmail.com>
2013-03-14 15:05:52 +0100

    add ajax support for reset_password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,6 +11,7 @@ class PasswordResetsController < ApplicationController
   def edit
     @user = User.load_from_reset_password_token(params[:id])
     @token = params[:id]
+
     not_authenticated unless @user
   end
 
