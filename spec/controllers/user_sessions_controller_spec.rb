5a9f0d96b220e621573e74c7699793af30a01430
Daniel Schmidt <dschmidt@weluse.de>
2013-03-14 16:34:58 +0100

    deleted unused, failing specs

--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -16,11 +16,4 @@ describe UserSessionsController do
     end
   end
 
-  describe "GET 'destroy'" do
-    it "returns http success" do
-      get 'destroy'
-      response.should be_success
-    end
-  end
-
 end
