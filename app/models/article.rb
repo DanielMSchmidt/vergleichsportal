49b118775fac64ab8d4db5500c4ddafe4bbbb010
Daniel Schmidt <dschmidt@weluse.de>
2013-03-14 16:33:59 +0100

    wsp

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,6 @@ class Article < ActiveRecord::Base
   validates_format_of :ean, with: /^((\d{7}-?\d{5}-?\d)|(\d{8}-?\d{4}-?\d)|(\d{9}-?\d{3}-?\d))$/
   validates_presence_of :description, :name
 
-
   has_many :article_cart_assignments
   has_many :article_query_assignments
   has_many :carts, through: :article_cart_assignments
