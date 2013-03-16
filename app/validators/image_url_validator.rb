66468ec3e95e04c4df0f4889b558dc2d0a74f75f
Daniel Schmidt <dschmidt@weluse.de>
2013-03-16 14:05:38 +0100

    fixed validation bug

--- app/validators/image_url_validator.rb
+++ app/validators/image_url_validator.rb
@@ -2,7 +2,7 @@
 class ImageUrlValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
 
-    unless value =~ /^(http:\/\/){0,1}(www.){0,1}\D*(\.png|\.jpg|\.bmp|\.png)$/
+    unless value =~ /^(http:\/\/){0,1}(www.){0,1}.*(\.png|\.jpg|\.bmp|\.png)$/
       record.errors[attribute] << (options[:message] || "is not an url")
     end
   end
