2a38741d5da23c54f1379a3244ab57d40e7f2b82
Daniel Schmidt <dschmidt@weluse.de>
2013-03-16 13:15:54 +0100

    fixed image_url validation

--- app/validators/image_url_validator.rb
+++ app/validators/image_url_validator.rb
@@ -1,7 +1,8 @@
 #encoding: utf-8
 class ImageUrlValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value =~ /^(http:\/\/){0,1}(www.){0,1}(\w|-|ö|ä|ü)*\.(\w){2,3}(\/(\/|\S)*){1}(\.png|\.jpg|\.bmp|\.png)$/
+
+    unless value =~ /^(http:\/\/){0,1}(www.){0,1}\D*(\.png|\.jpg|\.bmp|\.png)$/
       record.errors[attribute] << (options[:message] || "is not an url")
     end
   end
