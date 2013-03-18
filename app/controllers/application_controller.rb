d0e2240c18de10fb2cf11ef1461bc3a0f6c0175f
Daniel Schmidt <dschmidt@weluse.de>
2013-03-18 21:40:24 +0100

    fetching active users and carts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_filter :fetch_cart
-  before_filter :set_provider
+  before_filter :set_providers
+  before_filter :set_active_user
+  before_filter :set_active_cart
 
   def fetch_cart
     #TODO: Fill with something sensefull!
@@ -8,12 +10,22 @@ class ApplicationController < ActionController::Base
     @cart ||= Cart.create
   end
 
-  def active_user
+  def set_active_user
     @active_user ||= current_user ||= User.generateGuest
   end
 
-  def set_provider
+  def set_active_cart
+    @active_cart ||= fetchCartFromSession ||= @active_user.carts.create
+  end
+
+  def set_providers
      @providers = Provider.all
+  end
+
+  protected
+
+  def fetchCartFromSession
+    session[:active_cart]
   end
 
   protect_from_forgery
