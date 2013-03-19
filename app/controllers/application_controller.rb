47e6702ed5eb4ce1421313edd78947c0bef9eb1a
Tibor Weiß <info@tibor-weiss.de>
2013-03-19 14:28:32 +0100

    fetch_providers added to app controller, compare cart html converted to haml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_filter :fetch_cart, :fetch_add
+  before_filter :fetch_cart, :fetch_add, :fetch_providers
 
   def fetch_cart
     #TODO: Fill with something sensefull!
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
   def fetch_add
     @active_advertisment ||= Advertisment.where(:active => true).first
     @active_advertisment ||= Advertisment.new(:link_url => "http://placehold.it", :img_url => "http://placehold.it/205x300&text=Werbung.png", :active => true)
+  end
+
+  def fetch_providers
+    @providers = Provider.all
   end
 
   protect_from_forgery
