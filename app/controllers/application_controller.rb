1e3f3782f9a2c1aa88aaff2f28032513cd3df761
Daniel Schmidt <dschmidt@weluse.de>
2013-03-19 10:56:01 +0100

    added before filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   before_filter :set_providers
   before_filter :set_active_user
   before_filter :set_active_cart
+  before_filter :fetch_add
+  after_filter :setGuestUserInSession
 
 
   def set_active_user
@@ -17,10 +19,23 @@ class ApplicationController < ActionController::Base
      @providers = Provider.all
   end
 
+  def fetch_add
+    @active_advertisment ||= Advertisment.where(:active => true).first
+    @active_advertisment ||= Advertisment.create(:link_url => "http://tibor-weiss.de", :img_url => "http://tibor-weiss.de/Fotos/2012Schandmaul/Schandmaul/content/images/large/IMG_3408.jpg", :active => true)
+  end
+
   protected
 
   def fetchCartFromSession
     session[:active_cart]
+  end
+
+  def fetchUserFromSession
+    session[:guest_user_id]
+  end
+
+  def setGuestUserInSession
+    session[:guest_user_id] = @guest_user_id
   end
 
   protect_from_forgery
