324105db0ca0db905a5262c2ae71afe1d9e91869
Daniel Schmidt <dschmidt@weluse.de>
2013-03-19 15:05:31 +0100

    added specs for article add to cart

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Cart do
   let(:cart) { FactoryGirl.create(:cart) }
+  let(:article) { FactoryGirl.create(:article) }
 
   describe "relations" do
     it "should have many articles" do
@@ -14,6 +15,20 @@ describe Cart do
 
     it "should belong to a user" do
       should belong_to(:user)
+    end
+  end
+
+  describe "functions" do
+    describe "add_article" do
+      it "should add an assignment if none was allready there" do
+        ArticleCartAssignment.all.each{|x| x.delete}
+        expect{ cart.add_article(article) }.to change{ArticleCartAssignment.count}.by(1)
+      end
+      it "should increase the quanitity attribute of the assignment if one was there" do
+        expect{ cart.add_article(article) }.to change{ArticleCartAssignment.count}.by(1)
+        expect{ cart.add_article(article) }.to change{ArticleCartAssignment.count}.by(0)
+        expect{ cart.add_article(article) }.to change{ArticleCartAssignment.find_for_article_and_cart(article.id, cart.id).first.quantity}.by(1)
+      end
     end
   end
 end
