8c425f938932b22ece560970fe5910929cf92d06
Daniel Schmidt <dschmidt@weluse.de>
2013-03-20 16:09:52 +0100

    added database field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130319101258) do
+ActiveRecord::Schema.define(:version => 20130320150517) do
 
   create_table "advertisments", :force => true do |t|
     t.string   "img_url"
@@ -158,6 +158,7 @@ ActiveRecord::Schema.define(:version => 20130319101258) do
     t.string   "value"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.text     "options"
   end
 
   create_table "urls", :force => true do |t|
