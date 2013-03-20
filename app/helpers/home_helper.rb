1e1b6d715ca38b3907a7a68bdca7e012fa9ca97c
Daniel Schmidt <dschmidt@weluse.de>
2013-03-20 18:37:38 +0100

    added chart js to admin page

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,23 @@
 module HomeHelper
+
+  def getCompleteStatistics
+    statistics = {
+      labels: getLabels,
+      compares: getCompares,
+      searches: getSearches
+    }
+    return statistics
+  end
+
+  def getLabels
+    '"January","February","March","April","May","June","July"'
+  end
+
+  def getCompares
+    '65,59,90,81,56,55,40'
+  end
+
+  def getSearches
+    '28,48,40,19,96,27,100'
+  end
 end
