a6ee1c9e4ccc24ca3048b63f1ea4a16a8ea12ccb
David <davide135@gmail.com>
2013-03-21 14:25:48 +0100

    fix rating stars

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -56,19 +56,45 @@
 
 })(jQuery, this);
 
+
+
+
+
 function errornote(note,status){
 
   $("#errornote").append('<div class="alert-box '+ status +'">'+ note + '<a href="" class="close">&times;</a></div>');
 
 }
 
+
+function loadScript(scriptname) {  
+  var snode = document.createElement('script');  
+  snode.setAttribute('type','text/javascript');  
+  snode.setAttribute('src',scriptname);  
+  document.getElementsByTagName('head')[0].appendChild(snode);  
+}
+
+
 function showSearch(){
   $("#search-hidden").toggle();
   $("#search").toggle();
 }
 
-$('.auto-submit-star').rating({
-  callback: function(value, link){
-    this.form.submit();
-  }
+
+function activateStars(){
+
+  $('.auto-submit-star').rating({
+    callback: function(value, link){
+      this.form.submit();
+    }
+  });
+
+}
+
+function reloadStars(){
+  activateStars();
+}
+
+$(document).ready(function() {
+  activateStars();
 });
