3dad5b765a3d8f5fa7669d32b80ecdcc1ef71e1d
Tibor Weiß <info@tibor-weiss.de>
2013-03-21 20:34:31 +0100

    fixed the tests

--- app/validators/image_url_validator.rb
+++ app/validators/image_url_validator.rb
@@ -1,7 +1,7 @@
 #encoding: utf-8
 class ImageUrlValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value =~ /^(http:\/\/){1}(www.){0,1}.*(\.png|\.jpg|\.bmp|\.gif)$/
+    unless value =~ /^(http:\/\/){1}(www.){0,1}(\w|\.|-)*\.(\w){2,3}(\/.*){1}(\.png|\.jpg|\.bmp|\.gif)$/
       record.errors[attribute] << (options[:message] || "is not an url, needs a leading http:// and has to end with one of .png, .jpg, .bmp")
     end
   end
