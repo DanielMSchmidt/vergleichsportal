3a210056b84e85d67fcd37902f74fef52352aedf
Daniel Schmidt <dschmidt@weluse.de>
2013-03-21 16:32:12 +0100

    changed reload stars to reload location

--- app/views/carts/add_article.js
+++ app/views/carts/add_article.js
@@ -1,4 +1,4 @@
 $("#carts").html("<%= escape_javascript(render @active_cart.articles, search: false) %>");
 $("#change-cart").html("<%= escape_javascript(render @active_cart) %>"); 
 errornote("Artikel wurde hinzugefügt!", "success");
-reloadStars();
+location.reload();
--- app/views/carts/remove_article.js
+++ app/views/carts/remove_article.js
@@ -1,4 +1,4 @@
 $("#carts").html("<%= escape_javascript(render @active_cart.articles, search: false) %>");
 $("#change-cart").html("<%= escape_javascript(render @active_cart) %>");
 errornote("Artikel wurde entfernt!", "success");
-reloadStars();
+location.reload();
